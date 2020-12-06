de708911ef8cb902392515f94debb585e41b6e1b
SurajSSingh <surajss@uci.edu>
2020-12-05 23:26:18 -0800

    Settings Changes
    Changed Folder of Enemy States
    Wave Counter Visual
    Updated to 2019.4

--- Assets/Scripts/Enemy/SpawnManager.cs
+++ Assets/Scripts/Enemy/SpawnManager.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private int wave = 0;
+
     public List<EnemySpawner> spawnPoints = new List<EnemySpawner>();
+    public TextMeshProUGUI waveText;
 
     public void SpawnNewWave()
     {
-        foreach(EnemySpawner spawner in spawnPoints)
+        waveText.text = $"WAVE: {++wave}";
+        foreach (EnemySpawner spawner in spawnPoints)
         {
             spawner.SpawnEnemies();
         }
